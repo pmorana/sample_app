8b4db44d72eab6ff848c479b244f1d983530155a
Paul Morana <pmorana@gmail.com>
2016-01-28 17:05:11 -0600

    change Bucket to BUCKET

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -8,7 +8,7 @@ if Rails.env.production?
         :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
         :aws_secret_access_key => ENV['S3_SECRET_KEY'],
       }
-      config.fog_directory  = ENV['S3_Bucket']                             # required
+      config.fog_directory  = ENV['S3_BUCKET']                             # required
       config.fog_public     = false                                   # optional, defaults to true
       config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
     end
