641fb09d31f2fe6c2899e4543c862610a1c5d414
Paul Morana <pmorana@gmail.com>
2016-02-01 13:20:33 -0600

    Add followers and following and tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -80,4 +80,14 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "should follow and unfollow a user" do
+    paul = users(:paul)
+    archer = users(:archer)
+    assert_not paul.following?(archer)
+    paul.follow(archer)
+    assert paul.following?(archer)
+    assert archer.followers.include?(paul)
+    paul.unfollow(archer)
+    assert_not paul.following?(archer)
+  end
 end
